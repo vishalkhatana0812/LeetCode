caa4b327f6de4b3f4c42b83fb8bdc40a652bc9d4
vishal <vishalkhatana0812@gmail.com>
2022-04-15 21:38:39 +0530

    update

--- ARRAYS/Container_with_most_water.java
+++ ARRAYS/Container_with_most_water.java
@@ -3,7 +3,6 @@ package ARRAYS;
 public class Container_with_most_water {
     public static void main(String[] args) {
         int height[]={1,8,6,2,5,4,8,3,7};
-        System.out.println("hello");
         System.out.println(maxArea(height));
     }
     public static int maxArea(int[] height) {
